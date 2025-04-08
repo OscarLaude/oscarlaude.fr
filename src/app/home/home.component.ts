70254c06deb7916b9e935f0be3ce4864cb23fb66
OscarLaude <contact.laudeoscar@gmail.com>
2025-04-08 14:31:57 +0200

    career card + upgrades

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
 import { TuiIcon } from '@taiga-ui/core';
+import { TuiConnected, TuiStepper } from '@taiga-ui/kit';
 @Component({
   selector: 'app-home',
-  imports: [TuiIcon],
+  imports: [TuiIcon, NgFor, TuiConnected, TuiStepper],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  protected readonly experiences = [
+    {
+      role: 'Développeur Web Fullstack',
+      company: 'Magellium - avr.2025 - maintenant',
+    },
+    {
+      role: 'Développeur Web Fullstack Alternant',
+      company: 'Coyote - sept.2020 - sept.2024',
+    },
+  ];
 }
